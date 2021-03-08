7d1b054bc5dfc2d83804ec899458d0a4d2ec1c04
niro <Aziv19971111@gmail.com>
2021-03-07 21:13:05 -0400

    fix: 修改 Enum 引用

--- src/Enums/RoleSlug.php
+++ src/Enums/RoleSlug.php
@@ -2,13 +2,11 @@
 
 namespace Rbac\Enums;
 
-use Rbac\Enum\Enum;
+use MyCLabs\Enum\Enum;
 
 class RoleSlug extends Enum
 {
     const Administrator = 'administrator';  // 超管
-    const Designer      = 'designer';       // 设计人员
-    const Reviewer      = 'reviewer';       // 审核人员（版权员）
-    const Manager       = 'manager';        // 管理人员
-    const Leader        = 'leader';         // 最高管理人员（组长）
+    const Manager       = 'manager';
+    const Editor        = 'editor';
 }
