09d75a375a82207645838fedba7d974f9a835828
niro <Aziv19971111@gmail.com>
2021-03-07 21:23:56 -0400

    fix: 降低 mongodb 的版本

--- src/Enums/RoleSlug.php
+++ src/Enums/RoleSlug.php
@@ -6,7 +6,7 @@ use MyCLabs\Enum\Enum;
 
 class RoleSlug extends Enum
 {
-    const Administrator = 'administrator';  // 超管
+    const Administrator = 'administrator';
     const Manager       = 'manager';
     const Editor        = 'editor';
 }
